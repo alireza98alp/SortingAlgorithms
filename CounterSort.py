4f4e0437f61cb26105675e84b58ffcf1add3c427
Alireza <alireza77alp@gmail.com>
2022-12-22 15:09:43 +0330

    counter list completed
    still some changes needed

--- CounterSort.py
+++ CounterSort.py
@@ -1,12 +1,14 @@
 def CounterSort(list):
     lenght=len(list)
     maximum=max(list)
-    counter=[]
-    for i in range(maximum):
-        counter[i]=0
+    minimum=min(list)
+    counter=[0]*(maximum-minimum+1)
+    number=minimum
+    for i in range(maximum-minimum):
         for n in list:
-            if i==n:
-                counter+=1
+            if n==number:
+                counter[i]+=1
+        number+=1
     return counter
-list=[5,4,12,0,3]
+list=[5,4,1,5,6,3,3,3,3,3,3,2,1,0,0,0,2,2,2,2,12,0,3]
 print(CounterSort(list))
