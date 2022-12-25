1732b8b3e863d369d9a02b579a4b108f0c42c276
Alireza <alireza77alp@gmail.com>
2022-12-25 17:10:39 +0330

    function completed
    it's working correctly

--- CounterSort.py
+++ CounterSort.py
@@ -4,11 +4,15 @@ def CounterSort(list):
     minimum=min(list)
     counter=[0]*(maximum-minimum+1)
     number=minimum
-    for i in range(maximum-minimum):
+    for i in range(maximum-minimum+1):
         for n in list:
             if n==number:
                 counter[i]+=1
         number+=1
-    return counter
-list=[5,4,1,5,6,3,3,3,3,3,3,2,1,0,0,0,2,2,2,2,12,0,3]
-print(CounterSort(list))
+    list=[]
+    for k in range(len(counter)):
+        list += [minimum] * counter[k]
+        minimum+=1    
+    return list
+# list=[-2,5,4,1,5,6,3,3,3,3,3,3,2,1,0,0,0,2,2,2,2,12,0,3]
+# print(CounterSort(list))
